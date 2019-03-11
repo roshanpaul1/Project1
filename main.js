63cc0f6b8077db950132dd2230cc1a2e2f2c76e2
Roshan Paul <35750260+roshanpaul1@users.noreply.github.com>
2019-03-10 21:40:01 -0500

    Update main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+<script type="text/javascript">
+
+function submit() {
+         alert("form has been submitted!");
+
+}
+document.getElementById('submit').addEventListener('click', submit);
+
+</script>
