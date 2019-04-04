9ba8f4c2d998f5eeb9b6e6edd151154840a92d69
roshanpaul1 <35750260+roshanpaul1@users.noreply.github.com>
2019-04-04 15:13:50 -0500

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,15 @@
+// jQuery 3.x-style ready event and locally scoped $
+jQuery(function($) {
+  $('html').removeClass('nojs');
+  $('html').addClass('hasjs');
+});
 
+<script type="text/javascript">
+
+function submit() {
+         alert("form has been submitted!");
+
+}
+document.getElementById('submit').addEventListener('click', submit);
+
+</script>
