232bbe9c09ee0aec71d6f9ab6e7a88e0f6ef9dc9
Roshan Paul <35750260+roshanpaul1@users.noreply.github.com>
2019-04-12 15:15:04 -0500

    design

--- javascript/main.js
+++ javascript/main.js
@@ -4,7 +4,7 @@ jQuery(function($) {
   $('html').addClass('hasjs');
 });
 
-<script type="text/javascript">
+<script>
 
 function submit() {
          alert("form has been submitted!");
@@ -12,4 +12,25 @@ function submit() {
 }
 document.getElementById('submit').addEventListener('click', submit);
 
+</script>
+
+<script>
+function validateForm() {
+  var x = document.forms["myForm"]["firstname"].value;
+  if (x == "") {
+    alert("First Name must be filled out");
+	var y = document.forms["myForm"]["lastname"].value;
+  if (y == "") {
+    alert("Last Name must be filled out");
+	var z = document.forms["myForm"]["Birthday"].value;
+  if (z == "") {
+    alert("Birthday must be filled out");
+	var w = document.forms["myForm"]["Email"].value;
+  if (w == "") {
+    alert("Email must be filled out");
+	
+   
+  }
+   return false;
+}
 </script>
